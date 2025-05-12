fe5d0597695cf759a958f3f8414dc3c6a809c798
seyi <seyiduncan40@gmail.com>
2025-05-12 08:58:06 +0100

    lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import HeroSection from "./component/landingPage/hero";
 import ServicesSection from "./component/landingPage/serviceSection";
 import OtherServicesSection from "./component/landingPage/otherServices";
